262607feb9894c9d3e6dee87fa19cc1bc6069640
rusokverse <rusokverse@MACBOOKPRO>
2023-02-28 21:22:38 -0400

    Envío de objetos por ViewBag

--- Ejemplo_ASP_NET_MVC/Controllers/EjemplosController.cs
+++ Ejemplo_ASP_NET_MVC/Controllers/EjemplosController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ejemplo_ASP_NET_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 
 namespace Ejemplo_ASP_NET_MVC.Controllers
 {
@@ -16,6 +18,67 @@ namespace Ejemplo_ASP_NET_MVC.Controllers
             ViewBag.aprobado = true;
             ViewBag.calificacion = 67.5;
             ViewBag.fechaAprobacion = DateTime.Now;
+            return View();
+        }
+
+        public IActionResult EjemploPasarDatosObjeto()
+        {
+            var prod = new Producto {
+                Id = "atn",
+                Nombre = "Atún",
+                Foto = "atun.jpg",
+                Precio = 10.5,
+                Cantidad = 5
+            };
+            ViewBag.producto = prod;
+            return View();
+        }
+
+        public IActionResult EjemploPasarDatosObjetos()
+        {
+            var prods = new List<Producto> {
+                new Producto
+            {
+                Id = "atn",
+                Nombre = "Atún",
+                Foto = "atun.jpg",
+                Precio = 10.5,
+                Cantidad = 5
+            },
+                new Producto
+            {
+                Id = "qs",
+                Nombre = "Queso menonita",
+                Foto = "queso.jpg",
+                Precio = 25.0,
+                Cantidad = 8
+            },
+                new Producto
+            {
+                Id = "qs",
+                Nombre = "Queso menonita",
+                Foto = "uno.jpeg",
+                Precio = 25.0,
+                Cantidad = 8
+            },
+                new Producto
+            {
+                Id = "qs",
+                Nombre = "Queso menonita",
+                Foto = "dos.jpeg",
+                Precio = 25.0,
+                Cantidad = 8
+            },
+                new Producto
+            {
+                Id = "qs",
+                Nombre = "Queso menonita",
+                Foto = "tres.jpeg",
+                Precio = 25.0,
+                Cantidad = 8
+            }
+            };
+            ViewBag.productos = prods;
             return View();
         }
     }
---
+++ Ejemplo_ASP_NET_MVC/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejemplo_ASP_NET_MVC.Models
+{
+    public class Producto
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Foto { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
