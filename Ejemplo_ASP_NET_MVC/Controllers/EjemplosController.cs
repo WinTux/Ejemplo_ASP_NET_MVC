f43d7abbb2bf2e3c6a05b5e6ccda48393d1edbb8
rusokverse <rusokverse@MACBOOKPRO>
2023-02-28 20:48:54 -0400

    Manejo de archivos estáticos y ViewBag

---
+++ Ejemplo_ASP_NET_MVC/Controllers/EjemplosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Ejemplo_ASP_NET_MVC.Controllers
+{
+    public class EjemplosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EjemploPasarDatos()
+        {
+            ViewBag.edad = 13;
+            ViewBag.nombre = "Pepe";
+            ViewBag.aprobado = true;
+            ViewBag.calificacion = 67.5;
+            ViewBag.fechaAprobacion = DateTime.Now;
+            return View();
+        }
+    }
+}
---
+++ Ejemplo_ASP_NET_MVC/Controllers/EjemplosConttoller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejemplo_ASP_NET_MVC.Controllers
+{
+    public class EjemplosConttoller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
