4856ac3d0019a7cc6a1815ff944603b8c74eabbc
rusokverse <rusokverse@MACBOOKPRO>
2023-03-06 21:12:37 -0400

    File upload

---
+++ Ejemplo_ASP_NET_MVC/Controllers/SubirController.cs
@@ -0,0 +1,36 @@
+﻿using Ejemplo_ASP_NET_MVC.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace Ejemplo_ASP_NET_MVC.Controllers
+{
+    public class SubirController : Controller
+    {
+        IWebHostEnvironment webHostEnvironment;
+        public SubirController(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View("Index", new Producto());
+        }
+        public IActionResult Guardar(Producto producto, IFormFile foto)
+        {
+            if (foto == null || foto.Length == 0)
+                return Content("Archivo no elegido adecuadamente");
+            else
+            {
+                var ruta = Path.Combine(webHostEnvironment.WebRootPath, "imagenes", foto.FileName);
+                var stream = new FileStream(ruta, FileMode.Create);
+                foto.CopyToAsync(stream);
+                producto.Foto = foto.FileName;
+            }
+            ViewBag.producto = producto;
+
+            return View("Exito");
+        }
+    }
+}
