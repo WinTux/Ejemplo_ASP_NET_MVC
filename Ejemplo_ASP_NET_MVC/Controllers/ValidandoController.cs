f8967cee3a9430bab331f1600383156d1038df27
rusokverse <rusokverse@MACBOOKPRO>
2023-03-06 21:25:03 -0400

    Validación de formularios

---
+++ Ejemplo_ASP_NET_MVC/Controllers/ValidandoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejemplo_ASP_NET_MVC.Controllers
+{
+    public class ValidandoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ejemplo_ASP_NET_MVC/Models/CuentaUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ejemplo_ASP_NET_MVC.Models
+{
+    public class CuentaUsuario
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(15)]
+        public string usuario { get; set; }
+        //Continuar
+    }
+}
