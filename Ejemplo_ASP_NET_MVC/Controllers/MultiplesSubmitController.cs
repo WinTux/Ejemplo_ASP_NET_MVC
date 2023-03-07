dcde82624213a84d913f457c836e46ed5064237f
rusokverse <rusokverse@MACBOOKPRO>
2023-03-06 20:28:33 -0400

    Formularios multiples *

--- Ejemplo_ASP_NET_MVC/Controllers/MultiplesSubmitController.cs
+++ Ejemplo_ASP_NET_MVC/Controllers/MultiplesSubmitController.cs
@@ -2,11 +2,30 @@
 
 namespace Ejemplo_ASP_NET_MVC.Controllers
 {
+    [Route("multi")]
     public class MultiplesSubmitController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        [Route("destino1")]
+        public IActionResult Destino1()
+        {
+            return View("Destino1");
+        }
+        [HttpPost]
+        [Route("destino2")]
+        public IActionResult Destino2()
+        {
+            return View("Destino2");
+        }
+        [HttpPost]
+        [Route("destino3")]
+        public IActionResult Destino3()
+        {
+            return View("Destino3");
         }
     }
 }
