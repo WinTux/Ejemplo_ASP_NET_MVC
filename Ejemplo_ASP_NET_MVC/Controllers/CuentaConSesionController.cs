ee918b566a3141618cdbf93bfac29a36a2c377ca
rusokverse <rusokverse@MACBOOKPRO>
2023-03-09 20:39:57 -0400

    Eliminando sesión

---
+++ Ejemplo_ASP_NET_MVC/Controllers/CuentaConSesionController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ejemplo_ASP_NET_MVC.Controllers
+{
+    public class CuentaConSesionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string usuario, string password)
+        {
+            if (usuario != null && password != null && usuario.Equals("Pepe") && password.Equals("123456"))
+            {
+                //Guardar al usuario en una sesión
+                HttpContext.Session.SetString("usuario", usuario);
+                return View("Exito");
+            }
+            else {
+                ViewBag.error = "Credenciales incorrectas";
+                return View("Index");
+            }
+            
+        }
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("usuario");
+            return RedirectToAction("Index");
+        }
+    }
+}
